90ca8be4f347d193ecfe2a313dfcf54b4cd14afb
Gopikrishna471 <99220040583@klu.ac.in>
2025-03-25 10:41:44 +0530

    First commit
    Date:25-03-25

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form from reloading the page
+    alert("Thank you! We have received your message.");
+});
